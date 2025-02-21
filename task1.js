d50ef3cca4ad01fc6afd9db2b67f28ced3115df7
Olha Kalytyn <olhakalytyn@gmail.com>
2025-02-21 20:30:52 +0200

    complete tasks

--- task1.js
+++ task1.js
@@ -5,3 +5,19 @@
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
 // Перевірити за допомогою команди npm tests/task1.test.js 
+
+
+const ul = document.querySelector(".usersList");
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) =>
+    response.json().then((users) => {
+      users.forEach((user) => {
+        const li = document.createElement("li");
+        li.textContent = user.name;
+        ul.appendChild(li);
+      });
+    })
+  )
+  .catch((error) => {
+    console.error("Error fetching users:", error);
+  });
